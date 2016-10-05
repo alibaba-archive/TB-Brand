921fcc32910052d8ead0707bb5bbebeb57f94d0d
Sune Bear <hi@sunebear.com>
2016-10-05 21:40:35 +0800

    ISSUE(filed): Fixed typo

--- src/info.js
+++ src/info.js
@@ -4,7 +4,7 @@ import { mixinDeep } from './utils'
 export const en = {
   name: 'Teambition',
   slogan: 'Best collaboration tool for team.',
-  discription: 'Teambition is an easy and efficient way to work, by creating tasks, sharing posts and documents, keeping track of your calendar and checking everyone\'s progress.',
+  description: 'Teambition is an easy and efficient way to work, by creating tasks, sharing posts and documents, keeping track of your calendar and checking everyone\'s progress.',
   location: 'Changxing Building, 888 Bibo Road, Pudong District Shanghai, China',
   website: 'https://www.teambition.com',
   email: 'info@teambition.com',
@@ -22,7 +22,7 @@ export const en = {
 export const zh = mixinDeep({ founder: {} }, en, {
   name: 'Teambition',
   slogan: '最好用的团队协作工具',
-  discription: 'Teambition 是一个简单，高效的项目协作工具，你可以在这里管理项目，跟踪任务进度，存储项目文件，让你的团队协作更高效。',
+  description: 'Teambition 是一个简单，高效的项目协作工具，你可以在这里管理项目，跟踪任务进度，存储项目文件，让你的团队协作更高效。',
   industry: '互联网',
   location: '上海市浦东新区碧波路 888 号，畅星大厦',
   founder: {
